c771c37828610007d46fc09f44e00ecef7ef46ee
Rafael Niccioli <r.niccioli@gmail.com>
2023-05-06 13:08:42 -0300

    configurando pom para java 17

--- src/main/java/br/com/niccioli/App.java
+++ src/main/java/br/com/niccioli/App.java
@@ -4,10 +4,12 @@ package br.com.niccioli;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
+        var soma = 2;
+
+
         System.out.println( "Hello World!" );
     }
 }
